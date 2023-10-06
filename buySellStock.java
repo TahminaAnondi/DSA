bd570efec74c861b3d45ae68b832e156f1edf7f3
TahminaAnondi <tahmina.akter.anondi@gmail.com>
2023-10-05 20:38:53 -0500

    Solved buy and stock DSA problem

---
+++ buySellStock.java
@@ -0,0 +1,26 @@
+package DSA;
+
+import java.util.*;
+public class buySellStock{
+    public static void main(String args[]){
+        int stockPrice[] = {7,1,5,3,6,4};
+        System.out.println(buyAndSellStock(stockPrice));
+
+    }
+    public static int buyAndSellStock(int stockPrice[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit=0;
+        
+        for(int i=0;i<stockPrice.length; i++){
+            if(buyPrice<stockPrice[i]){//profit
+                int profit = stockPrice[i]-buyPrice;//today's profit
+                maxProfit = Math.max(maxProfit,profit);
+            }else{
+                buyPrice = stockPrice[i];
+            }
+        }
+        return maxProfit;
+
+    }
+
+}
