5b3daac62dc7efd86df19ee1bd2157629ff60f44
TahminaAnondi <tahmina.akter.anondi@gmail.com>
2023-10-07 11:30:31 -0500

    fixed bugs

--- selectionSort.java
+++ selectionSort.java
@@ -8,14 +8,17 @@ public class selectionSort {
     public static void selectionSortName(int unsortedArray[]){
         
         for(int i=0;i<unsortedArray.length-1;i++)
-        {            
+        {        
+            //Assume the current element    
+            int minIndex = i;
             for(int j=i+1;j<unsortedArray.length;j++){
-                if(unsortedArray[i]>unsortedArray[j]){
-                    int temp = unsortedArray[j];
-                    unsortedArray[j] = unsortedArray[i];
-                    unsortedArray[i] = temp;
+                if(unsortedArray[minIndex]>unsortedArray[j]){
+                    minIndex = j;
                 }
             }
+                int temp = unsortedArray[minIndex];
+                unsortedArray[minIndex] = unsortedArray[i];
+                unsortedArray[i] = temp;
         }
         for(int i=0;i<unsortedArray.length;i++){
             System.out.print(unsortedArray[i]+", ");
